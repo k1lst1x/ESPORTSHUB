406efba98620ce2fca5eec04da97435c6f2573ce
saint <damir.dzhubaliev@gmail.com>
2025-05-10 14:42:58 +0600

    fix

--- news/views.py
+++ news/views.py
@@ -9,26 +9,29 @@ def news_list(request):
     all_news = News.objects.all()
     return render(request, 'news_list.html', {'all_news': all_news})
 
-@login_required
 def news_detail(request, news_id):
     """Страница с подробной информацией о конкретной новости и комментариями"""
     news_item = get_object_or_404(News, id=news_id)
     comments = Comment.objects.filter(news=news_item)
 
     if request.method == 'POST':
-        comment_text = request.POST.get('comment_text', '').strip()
-        if comment_text:
-            Comment.objects.create(
-                news=news_item,
-                user=request.user,
-                text=comment_text
-            )
+        if request.user.is_authenticated:
+            comment_text = request.POST.get('comment_text', '').strip()
+            if comment_text:
+                Comment.objects.create(
+                    news=news_item,
+                    user=request.user,
+                    text=comment_text
+                )
 
-            # ✅ Начисляем очки пользователю
-            request.user.points += POINTS_FOR_COMMENT
-            request.user.save()
+                # ✅ Начисляем очки пользователю
+                request.user.points += POINTS_FOR_COMMENT
+                request.user.save()
 
-            return redirect('news_detail', news_id=news_id)
+        else:
+            return redirect('login')
+
+        return redirect('news_detail', news_id=news_id)
 
     return render(request, 'news_detail.html', {
         'news_item': news_item,
